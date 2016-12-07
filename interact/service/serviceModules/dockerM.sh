a7d0fdd99493ce2e542f75102a930e3a0f3b8f74
Michael Czaja <MichaelCzaja@fico.com>
2016-12-07 16:50:31 +0100

    spark compose PART2

--- interact/service/serviceModules/dockerM.sh
+++ interact/service/serviceModules/dockerM.sh
@@ -16,6 +16,9 @@ if [ "$1" == "build all images" ]
 		docker build -t futureapplications/streaming-kafka ../../../images/kafka/.
 
 		docker build -t futureapplications/streaming-zookeeper ../../../images/zookeeper/.
+
+		docker build -t futureapplications/spark-compose-2.0 ../../../images/spark2.0-compose/.
+
 	exit 0
 fi
 
