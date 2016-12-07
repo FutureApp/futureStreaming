1909095e6271152e624c52db4486ae0c78d1d310
Michael Czaja <MichaelCzaja@fico.com>
2016-12-07 19:00:40 +0100

    a

--- interact/fu_service.sh
+++ interact/fu_service.sh
@@ -8,7 +8,7 @@ if [ "$1" == "start sparkCluster" ] && [ -z != $2 ] && [ -z != $3 ] && [ -z != $
 	exit 0
 fi
 
-if [ "$1" == "start spark-compose" ]  
+if [ "$1" == "start spark-compose" ]
 	then
 	bash ./fu_service_functions.sh $1
 	exit 0
