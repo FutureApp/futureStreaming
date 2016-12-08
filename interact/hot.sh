c5a11c3efb8d31627113cbad3c6d983689676499
Michael Czaja <MichaelCzaja@fico.com>
2016-12-08 18:40:31 +0100

    install mongo

--- interact/hot.sh
+++ interact/hot.sh
@@ -4,8 +4,10 @@ iHome="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd $iHome
 
 
-if [ "$1" == "start mongo" ]
+if [ "$1" == "mongo" ]
 	then
+    bash ./fu_service_functions.sh "kill all container"
 	sudo docker build -t futureapplications/streaming-mongo ../images/mongo/.
+    docker run -d --name "mongo" -h  "mongo" -v futureapplications/streaming-mongo
 	exit 0
 fi
