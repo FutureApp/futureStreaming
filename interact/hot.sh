8cf009d137a1732047cc54fae430f3c638afb51a
Michael Czaja <MichaelCzaja@fico.com>
2016-12-13 14:15:30 +0100

    mongo hot-functions

--- interact/hot.sh
+++ interact/hot.sh
@@ -3,11 +3,11 @@ NC='\033[0m' # No Color
 iHome="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd $iHome
 
-
 if [ "$1" == "mongo" ]
 	then
     bash ./fu_service_functions.sh "kill all container"
-	docker build -t futureapplications/streaming-mongo ../images/mongo/.
-    docker run -d -p 8080:8080 --name "mongo" -h  "mongo"  futureapplications/streaming-mongo
+	docker build -t futureapplications/mongodb-v3 ../images/mongo/.
+	cd ../images/mongo
+    docker-compose up
 	exit 0
 fi
