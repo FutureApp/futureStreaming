54df56e4776a6359a26a024ae7f8d288f0672455
Michael Czaja <MichaelCzaja@fico.com>
2016-12-13 14:09:12 +0100

    populate functions for mongodb

--- interact/service/serviceModules/mongo.sh
+++ interact/service/serviceModules/mongo.sh
@@ -3,9 +3,11 @@ NC='\033[0m' # No Color
 iHome="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd $iHome
 
-if [ -z != $1 ]
+if [ "mongo" != $1 ]
 	then
-	
+	bash ./dockerM.sh "kill container" "mongodbv-3"
+	cd ../../../images/mongo
+	compose-up
 	exit 0
 fi
 
